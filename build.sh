c9f167bd45e56cba3b0b19bf06a1b34714c72579
Tiemen Schuijbroek <t.j.l.schuijbroek@ratio-case.nl>
2024-11-18 12:42:57 +0100

    Remove leftover sudo

--- build.sh
+++ build.sh
@@ -24,5 +24,5 @@ RELEASE="$(rpm -E %fedora)"
 # Install Firefox and the PWA extension.
 rpm-ostree install firefox
 rpm --import https://packagecloud.io/filips/FirefoxPWA/gpgkey
-echo -e "[firefoxpwa]\nname=FirefoxPWA\nmetadata_expire=300\nbaseurl=https://packagecloud.io/filips/FirefoxPWA/rpm_any/rpm_any/\$basearch\ngpgkey=https://packagecloud.io/filips/FirefoxPWA/gpgkey\nrepo_gpgcheck=1\ngpgcheck=0\nenabled=1" | sudo tee /etc/yum.repos.d/firefoxpwa.repo
+echo -e "[firefoxpwa]\nname=FirefoxPWA\nmetadata_expire=300\nbaseurl=https://packagecloud.io/filips/FirefoxPWA/rpm_any/rpm_any/\$basearch\ngpgkey=https://packagecloud.io/filips/FirefoxPWA/gpgkey\nrepo_gpgcheck=1\ngpgcheck=0\nenabled=1" | tee /etc/yum.repos.d/firefoxpwa.repo
 rpm-ostree install firefoxpwa
