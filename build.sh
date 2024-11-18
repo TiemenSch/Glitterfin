b62a00070e74958b5aec9f1dfba34c7c05831450
Tiemen Schuijbroek <t.j.l.schuijbroek@ratio-case.nl>
2024-11-18 12:06:53 +0100

    Switch to RPM Firefox

--- build.sh
+++ build.sh
@@ -13,11 +13,17 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen
+# rpm-ostree install screen
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
 
 #### Example for enabling a System Unit File
+# systemctl enable podman.socket
 
-systemctl enable podman.socket
+flatpak remove org.mozilla.firefox
+rpm-ostree install firefox
+
+rpm --import https://packagecloud.io/filips/FirefoxPWA/gpgkey
+echo -e "[firefoxpwa]\nname=FirefoxPWA\nmetadata_expire=300\nbaseurl=https://packagecloud.io/filips/FirefoxPWA/rpm_any/rpm_any/\$basearch\ngpgkey=https://packagecloud.io/filips/FirefoxPWA/gpgkey\nrepo_gpgcheck=1\ngpgcheck=0\nenabled=1" | sudo tee /etc/yum.repos.d/firefoxpwa.repo
+rpm-ostree install firefoxpwa
