cc48523d005f66ac63774fe3b113d79358bb9df1
Tiemen Schuijbroek <t.j.l.schuijbroek@ratio-case.nl>
2026-03-15 18:41:50 +0100

    Update build process to utilize bootc
    Signed-off-by: Tiemen Schuijbroek <t.j.l.schuijbroek@ratio-case.nl>

---
+++ build_files/build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+### Install packages
+
+# Packages can be installed from any enabled yum repo on the image.
+# RPMfusion repos are available by default in ublue main images
+# List of rpmfusion packages can be found here:
+# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
+
+# this installs a package from fedora repos
+dnf5 install -y \
+  btrbk \
+  darktable \
+  firefox \
+  libffi-devel \
+  libxslt-devel \
+  libxml2-devel
+
+# Install Firefox PWA
+tee /etc/yum.repos.d/firefoxpwa.repo > /dev/null <<EOF
+[firefoxpwa]
+name=FirefoxPWA
+metadata_expire=7d
+baseurl=https://packagecloud.io/filips/FirefoxPWA/rpm_any/rpm_any/\$basearch
+gpgkey=https://packagecloud.io/filips/FirefoxPWA/gpgkey
+       https://packagecloud.io/filips/FirefoxPWA/gpgkey/filips-FirefoxPWA-912AD9BE47FEB404.pub.gpg
+repo_gpgcheck=1
+gpgcheck=1
+enabled=1
+EOF
+dnf5 -q makecache -y --disablerepo="*" --enablerepo="firefoxpwa"
+dnf5 install -y firefoxpwa
+
+# Use a COPR Example:
+#
+# dnf5 -y copr enable ublue-os/staging
+# dnf5 -y install package
+# Disable COPRs so they don't end up enabled on the final image:
+# dnf5 -y copr disable ublue-os/staging
+
+#### Example for enabling a System Unit File
+
+systemctl enable podman.socket
